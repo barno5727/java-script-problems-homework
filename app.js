fbbce216afca8e6f4d5855d3e1da2f649f9fd97e
unknown <barnoodilovna7405727@mail.ru>
2023-01-20 02:03:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,133 @@
+// Mantiqiy amallar
+// 1-masala
+
+// let a = 16;
+
+// if (0 > a == 0) {
+//     console.log("Musbat son");
+// } else {
+//     console.log("Manfiy son");
+// }
+
+
+
+// 2-masala
+// 3-masala
+
+// let b = 20 ;
+
+// if (b % 2 == 0) {
+//     console.log("Juft son");
+// }  else {
+//     console.log("Toq son");
+// }
+
+
+// 4-masala 
+
+// let a = 20 ;
+// let b = 22 ;
+
+// if (a > 0 && b > 0) {
+//     console.log("True");
+// }  else {
+//     console.log("False");
+// }
+
+
+// 5 6-masala 
+
+// let a = 22;
+// let b = 20;
+// let c = 18;
+
+// if (a>b && b>c) {
+//     console.log("True");
+// }  else {
+//     console.log("False");
+// }
+
+
+// 7-masala 
+
+// let a = 22;
+// let b = 20;
+
+// if (b , a % 2 == 0) {
+//         console.log("Juft son");
+//     }  else (a, b % 3 == 0) 
+//     {
+//         console.log("Toq son");
+
+//     }
+    
+
+// 8  9 - masala
+
+// let a = 10;
+// let b = 12;
+// let c = 14;
+// let d = 16;
+
+// if (a, b, c, d % 2 == 0) {
+//         console.log("Juft son");
+//     }  else {
+//         console.log("Toq son");
+//     }
+
+
+// 9 - masala
+
+
+// let a = 10;
+// let b = 13;
+// let c = 14;
+// let d = 17;
+
+// if (a, b, c, d % 2 == 0) {
+//         console.log("Juft son");
+//     }  else {
+//         console.log("Toq son");
+//     }
+
+
+// 10 - masala
+
+//   let day =2;
+
+//   if (day == 1) {
+//     console.log("Dushanba")
+//   } else if (day == 2) {
+//     console.log("Seshanba")
+//   } else if ( day == 3) {
+//     console.log("Chorshanba")
+//   } else if ( day == 4) {
+//     console.log("Payshanba")
+//   }
+//     else if ( day == 5) {
+//         console.log("Juma")   
+//     }
+//   else if ( day == 6) {
+//     console.log("Shanba") 
+//   } else if ( day == 7) {
+//     console.log("Yakshanba")
+//   } else ("Bunday kun mavjud emas") ;
+
+
+// 11 - masala 
+
+// let cod = 93;
+
+// if (cod == 71) {
+//     console.log("Domashniy") 
+// } else if ( cod == 97) {
+//     console.log("UMS") 
+// } else if (cod == 98) {
+//     console.log("Perfectum Mobile") 
+// } else if ( cod == 99) {
+//     console.log("UZMOBILE") 
+// } else if ( cod == 93, 94) {
+//     console.log("Usell") 
+// } else if ( cod == 90) {
+//     console.log("Beeline") 
+// } else ("Bunday aloqa kompaniyasi mavjud emas")
